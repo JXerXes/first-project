60a10b8e86a2b8d0e7a1ffd6bd0f59501443869d
user1 <user1@BFEI-208-10.BFEI.ie>
2024-02-05 11:04:29 +0000

    test

--- Program.cs
+++ Program.cs
@@ -6,12 +6,13 @@ namespace toDo_list
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main() 
         {
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
+            // test one
         }
     }
 }
